b57faad9735c7ebda6a80b8040f7c9cd379a60d6
manneshiva <manneshiva+github@gmail.com>
2017-06-27 02:56:24 +0530

    renaming

--- nn_frameworks/gensim/gensim_w2v_benchmark.py
+++
@@ -1,8 +0,0 @@
-import gensim
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-text8_file = '../../data/text8'
-sentences = gensim.models.word2vec.Text8Corpus(text8_file)
-print(sentences)
-model = gensim.models.Word2Vec(sentences, workers=6)
