6b4d0ae7f2d55d1b010b7e323e98cd87b65dbb91
ivan <menshikh.iv@gmail.com>
2017-09-17 14:59:31 +0500

    Fix E501 for wiki2corpus

--- wiki2corpus.py
+++ wiki2corpus.py
@@ -14,18 +14,24 @@ from gensim.corpora import WikiCorpus
 import snowballstemmer
 
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--wikifile', help='Path to wiki dump xml', required=True)
     parser.add_argument('--corpusfile', help='Path to output corpus file', required=True)
-    parser.add_argument('--words_per_line', default=1000, type=int, help='Number of words on each line. Default : %(default)s')
+    parser.add_argument(
+        '--words_per_line', default=1000, type=int, help='Number of words on each line. Default : %(default)s'
+    )
     return parser.parse_args()
+
 
 if __name__ == '__main__':
 
     options = parse_args()
     stemmer = snowballstemmer.stemmer('english')
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     i = 0
     wiki = WikiCorpus(options.wikifile, lemmatize=False)
     with open(options.corpusfile, 'w') as output:
@@ -34,5 +40,5 @@ if __name__ == '__main__':
             while words < len(text):
                 output.write(' '.join(stemmer.stemWords(text[words:words + options.words_per_line])) + "\n")
                 words += options.words_per_line
-            if (i % 10000 == 0):
-                logging.info('Saved %d articles', i)
+            if i % 10000 == 0:
+                logger.info('Saved %d articles', i)
