9cd32fc48fb6af9db89264a18cbfe00890300b63
ivan <menshikh.iv@gmail.com>
2017-09-17 14:58:58 +0500

    PEP8 for gensim_w2v

--- nn_frameworks/gensim/gensim_word2vec.py
+++ nn_frameworks/gensim/gensim_word2vec.py
@@ -13,17 +13,45 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', help='Path to text corpus', required=True)
     parser.add_argument('--outputpath', help='Path to save trained vectors', required=True)
-    parser.add_argument('--iter', default=5, type=int, help='Number of iterations (epochs) over the corpus. Default : %(default)s')
-    parser.add_argument('--size', default=100, type=int, help='Dimensionality of the embeddings/feature vectors. Default : %(default)s')
-    parser.add_argument('--window', default=5, type=int, help='Maximum distance between the current and predicted word within a sentence. Default : %(default)s')
-    parser.add_argument('--min_count', default=5, type=int, help='This will discard words that appear less than MIN_COUNT times. Default : %(default)s')
-    parser.add_argument('--workers', default=3, type=int, help='Use these many worker threads to train the model. Default : %(default)s')
-    parser.add_argument('--sample', default=1e-3, type=float, help='Set threshold for occurrence of words. Those that appear with higher frequency \
-        in the training data will be randomly down-sampled; default is %(default)s, useful range is (0, 1e-5)')
-    parser.add_argument('--sg', default=1, choices=[0, 1], type=int, help='Use the skip-gram model; default is %(default)s (use 0 for continuous bag of words model)')
-    parser.add_argument('--negative', default=5, type=int, help='Number of negative examples; default is %(default)s, common values are 3 - 10 (0 = not used)')
-    parser.add_argument('--alpha', default=0.025, type=float, help='The initial learning rate. Default : %(default)s')
+    parser.add_argument(
+        '--iter', default=5, type=int,
+        help='Number of iterations (epochs) over the corpus. Default : %(default)s'
+    )
+    parser.add_argument(
+        '--size', default=100, type=int,
+        help='Dimensionality of the embeddings/feature vectors. Default : %(default)s'
+    )
+    parser.add_argument(
+        '--window', default=5, type=int,
+        help='Maximum distance between the current and predicted word within a sentence. Default : %(default)s'
+    )
+    parser.add_argument(
+        '--min_count', default=5, type=int,
+        help='This will discard words that appear less than MIN_COUNT times. Default : %(default)s'
+    )
+    parser.add_argument(
+        '--workers', default=3, type=int,
+        help='Use these many worker threads to train the model. Default : %(default)s'
+    )
+    parser.add_argument(
+        '--sample', default=1e-3, type=float,
+        help='Set threshold for occurrence of words. Those that appear with higher frequency'
+        'in the training data will be randomly down-sampled; default is %(default)s, useful range is (0, 1e-5)'
+    )
+    parser.add_argument(
+        '--sg', default=1, choices=[0, 1], type=int,
+        help='Use the skip-gram model; default is %(default)s (use 0 for continuous bag of words model)'
+    )
+    parser.add_argument(
+        '--negative', default=5, type=int,
+        help='Number of negative examples; default is %(default)s, common values are 3 - 10 (0 = not used)'
+    )
+    parser.add_argument(
+        '--alpha', default=0.025, type=float,
+        help='The initial learning rate. Default : %(default)s'
+    )
     return parser.parse_args()
+
 
 if __name__ == '__main__':
 
