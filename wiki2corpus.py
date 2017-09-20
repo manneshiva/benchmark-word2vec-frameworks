eb10115518c2dccd7367b740adb1f7cd4510ff07
manneshiva <manneshiva+github@gmail.com>
2017-09-21 00:50:13 +0530

    remove stemmer

--- wiki2corpus.py
+++ wiki2corpus.py
@@ -11,7 +11,6 @@ Construct a corpus from a Wikipedia (or other MediaWiki-based) database dump.
 import argparse
 import logging
 from gensim.corpora import WikiCorpus
-import snowballstemmer
 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -23,7 +22,7 @@ def parse_args():
     parser.add_argument('--wikifile', help='Path to wiki dump xml', required=True)
     parser.add_argument('--corpusfile', help='Path to output corpus file', required=True)
     parser.add_argument(
-        '--words_per_line', default=1000, type=int, help='Number of words on each line. Default : %(default)s'
+        '--words_per_line', default=10000, type=int, help='Number of words on each line. Default : %(default)s'
     )
     return parser.parse_args()
 
@@ -31,14 +30,12 @@ def parse_args():
 if __name__ == '__main__':
 
     options = parse_args()
-    stemmer = snowballstemmer.stemmer('english')
-    i = 0
     wiki = WikiCorpus(options.wikifile, lemmatize=False)
     with open(options.corpusfile, 'w') as output:
         for i, text in enumerate(wiki.get_texts()):
             words = 0
             while words < len(text):
-                output.write(' '.join(stemmer.stemWords(text[words:words + options.words_per_line])) + "\n")
+                output.write(' '.join(text[words:words + options.words_per_line]) + "\n")
                 words += options.words_per_line
             if i % 10000 == 0:
                 logger.info('Saved %d articles', i)
